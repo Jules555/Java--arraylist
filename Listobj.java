d55c00ef744ac6cefaab91e574651cfa6b44b16a
UMUHIRE JULES <46833566+Jules555@users.noreply.github.com>
2019-12-19 16:02:39 +0800

    Update Listobj.java

--- Listobj.java
+++ Listobj.java
@@ -3,39 +3,27 @@ import java.util.ArrayList;
 public class Listobj {
     public static void main (String args[]){
 
-       // ArrayList<String> Shoes = new ArrayList<>();
+     //Creating object for class List which has our methods.
         List o1 = new List();
 
 
-        String f = "";
+        o1.setOwner("Jules ");
 
 
-        o1.setOwner("Jules own the following shoes");
-
-       // o1.getOwner();
-
+        //assign values to our Array
         o1.setShoes("Nike");
         o1.setShoes("FiLA");
         o1.setShoes("Puma");
         o1.setShoes("cat");
         o1.setShoes("timber");
-
-
+        
+        //we check if everything is working well
         System.out.println(o1.getOwner());
         System.out.println(o1.getShoes());
 
-      /*  for(int i=0; i<o1.Shoes.size();i++){
 
-                String brand =  o1.Shoes.get(i);
-
-                f +=brand+" ";
-
-        }
-
-        o1.getShoes(f);
-
-        System.out.println(o1.getShoes(f));*/
 
 
     }
 }
+
